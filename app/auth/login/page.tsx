73267708a151189ac194fe9618fb0515c6da7e53
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-05-26 16:38:05 +0330

    create the login page and make the layout responsive

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="bg-white xl:bg-primary min-h-screen min-w-screen flex">
+      <div className="login w-full bg-white xl:w-1/2 xl:m-4 xl:rounded-xl xl:p-3">form</div>
+      <div className="animation hidden xl:flex xl:w-1/2">animation</div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body className={`${Yekan.className} ${Yekan.variable}`}>{children}</body>
     </html>
   );
