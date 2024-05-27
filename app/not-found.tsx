b2c68b6867a4f04e25ee0e1dd2b2df13883ed475
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-05-27 21:13:43 +0330

    justify text in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,8 +10,10 @@ const NotFound = () => {
     <div className="min-w-screen min-h-screen overflow-hidden flex flex-col justify-center items-center align-middle">
       <div className="flex flex-col max-w-sm space-y-5 p-4">
         <Lottie animationData={animation} loop={true} />
-        <h1 className="text-6xl font-semibold leading-normal">صفحه مورد نظر پیدا نشد !</h1>
-        <p>
+        <h1 className="text-6xl font-semibold leading-normal">
+          صفحه مورد نظر پیدا نشد !
+        </h1>
+        <p className="text-justify">
           لطفا به آدرسی که وارد کردید دقت کنید و یا اگر بصورت اتوماتیک وارد این
           صفحه شدید با پشتیبانی تماس بگیرید.
         </p>
